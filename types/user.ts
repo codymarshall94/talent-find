a8199bb45c9f58a17bf5ef7392ec9db052314838
Cody Marshall <codymstuntsx@gmail.com>
2023-09-13 12:31:31 -0700

    UPDATED: serperated types

--- types/user.ts
+++ types/user.ts
@@ -1,50 +1,82 @@
-interface Stunt {
+interface ContactInformation {
+  email: string;
+  phone: string;
+}
+
+interface Location {
+  city: string;
+  province_state: string;
+  country: string;
+}
+
+interface Links {
+  website: string;
+  imdb: string;
+  instagram: string;
+  youtube: string;
+}
+
+interface Attributes {
+  gender: string;
+  ethnicity: string;
+  age_range: string;
+  build: string;
+  hair_color: string;
+  eye_color: string;
+  height: string;
+  weight: string;
+  physique: string;
+}
+
+interface Measurements {
+  waist: string;
+  inseam: string;
+  chest: string;
+  neck: string;
+  shoe: string;
+}
+
+interface Skills {
+  aquatic: string[];
+  combat: string[];
+  fire: string[];
+  sport: string[];
+  vehicle: string[];
+}
+
+interface Representation {
+  agency_name: string;
+  agent_name: string;
+}
+
+interface Credit {
   media: string;
   role: string;
   reference: string;
 }
 
-interface User {
-  name: string;
-  title: string;
-  ageRange: number;
-  contact: {
-    mobile: string;
-    address: string;
-    email: string;
-  };
-  links: {
-    website: string;
-    imdb: string;
-    instagram: string;
-    youtube: string;
-  };
-  body: {
-    height: number;
-    weight: number;
-    hairColor: string;
-    eyeColor: string;
-    waist: number;
-    inseam: number;
-    chest: number;
-    neck: number;
-    shoe: number;
-    physique: string;
-  };
+interface Credits {
+  film: Credit[];
+  tv: Credit[];
+  commercial: Credit[];
+}
+
+export interface User {
+  user_id: string;
+  user_title: string;
+  first_name: string;
+  avatar: string;
+  last_name: string;
+  bio: string;
   photos: string[];
-  skills: {
-    aquatic?: string[];
-    firearms?: string[];
-    combat?: string[];
-    sports?: string[];
-    extreme?: string[];
-    vehicles?: string[];
-  };
-  credits: {
-    film?: Stunt[];
-    tv?: Stunt[];
-    commercial?: Stunt[];
-  };
-  unionStatus: string;
-  affiliation: string;
+  contact_information: ContactInformation;
+  location: Location;
+  links: Links;
+  attributes: Attributes;
+  measurements: Measurements;
+  skills: Skills;
+  representation: Representation;
+  credits: Credits;
+  union_affiliations: string;
+  driver_license: boolean;
 }
