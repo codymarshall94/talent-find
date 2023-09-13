7e8a03fe78f9ffb38b6bc06b9b37d0770f2c71a8
Cody Marshall <codymstuntsx@gmail.com>
2023-09-13 12:27:04 -0700

    IMPORTED: home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+import Home from "@/scenes/home/Home";
+
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+      <Home />
     </main>
-  )
+  );
 }
