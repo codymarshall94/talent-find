8a10decf0a9d38d5265c91cd4d4adae65c73d37a
Cody Marshall <codymstuntsx@gmail.com>
2023-09-13 12:30:56 -0700

    CREATED: constants for for selects

---
+++ constants/form.ts
@@ -0,0 +1,300 @@
+const jobTypes = [
+  {
+    label: "Stunts",
+    value: "stunts",
+  },
+  {
+    label: "Stunt Coordinator",
+    value: "stunt_coordinator",
+  },
+  {
+    label: "Fight Coordinator",
+    value: "fight_coordinator",
+  },
+  {
+    label: "Stunt Rigger",
+    value: "stunt_rigger",
+  },
+];
+
+const skillTypes = {
+  aquatic: [
+    {
+      label: "Scuba Diving",
+      value: "scuba_diving",
+    },
+    {
+      label: "Swimming",
+      value: "swimming",
+    },
+    {
+      label: "Water Skiing",
+      value: "water_skiing",
+    },
+    {
+      label: "Jet Skiing",
+      value: "jet_skiing",
+    },
+    {
+      label: "Surfing",
+      value: "surfing",
+    },
+    {
+      label: "Boating",
+    },
+  ],
+  combat: [
+    {
+      label: "Boxing",
+      value: "boxing",
+    },
+    {
+      label: "Martial Arts",
+      value: "martial_arts",
+    },
+    {
+      label: "Kickboxing",
+      value: "kickboxing",
+    },
+    {
+      label: "Muay Thai",
+      value: "muay_thai",
+    },
+    {
+      label: "Judo",
+      value: "judo",
+    },
+    {
+      label: "Karate",
+      value: "karate",
+    },
+  ],
+  fire: [
+    {
+      label: "Burns",
+      value: "burns",
+    },
+  ],
+  sport: [
+    {
+      label: "Basketball",
+      value: "basketball",
+    },
+    {
+      label: "Baseball",
+      value: "baseball",
+    },
+    {
+      label: "Gymnastics",
+      value: "gymnastics",
+    },
+    {
+      label: "Parkour",
+      value: "parkour",
+    },
+    {
+      label: "Wrestling",
+      value: "wrestling",
+    },
+    {
+      label: "Skateboarding",
+      value: "skateboarding",
+    },
+    {
+      label: "Snowboarding",
+      value: "snowboarding",
+    },
+    {
+      label: "Surfing",
+      value: "surfing",
+    },
+  ],
+  vehicle: [
+    {
+      label: "Motorcycle",
+      value: "motorcycle",
+    },
+    {
+      label: "Car Jumps",
+      value: "car_jumps",
+    },
+    {
+      label: "Car Hits",
+      value: "car_hits",
+    },
+    {
+      label: "Car Rolls",
+      value: "car_rolls",
+    },
+    {
+      label: "Precision Driver",
+      value: "precision_driver",
+    },
+    {
+      label: "Drifting",
+      value: "drifting",
+    },
+  ],
+};
+
+const ageRange = [
+  {
+    label: "18-24",
+    value: "18-24",
+  },
+  {
+    label: "25-34",
+    value: "25-34",
+  },
+  {
+    label: "35-44",
+    value: "35-44",
+  },
+  {
+    label: "45-54",
+    value: "45-54",
+  },
+  {
+    label: "55-64",
+    value: "55-64",
+  },
+  {
+    label: "65+",
+    value: "65+",
+  },
+];
+
+const ethnicity = [
+  {
+    label: "Caucasion",
+    value: "caucasion",
+  },
+  {
+    label: "African American",
+    value: "african_american",
+  },
+  {
+    label: "Hispanic",
+    value: "hispanic",
+  },
+  {
+    label: "Asian",
+    value: "asian",
+  },
+  {
+    label: "Native American",
+    value: "native_american",
+  },
+  {
+    label: "Other",
+    value: "other",
+  },
+];
+
+const hairColor = [
+  {
+    label: "Black",
+    value: "black",
+  },
+  {
+    label: "Brown",
+    value: "brown",
+  },
+  {
+    label: "Blonde",
+    value: "blonde",
+  },
+  {
+    label: "Red",
+    value: "red",
+  },
+  {
+    label: "Grey",
+    value: "grey",
+  },
+  {
+    label: "Other",
+    value: "other",
+  },
+];
+
+const eyeColor = [
+  {
+    label: "Brown",
+    value: "brown",
+  },
+  {
+    label: "Blue",
+    value: "blue",
+  },
+  {
+    label: "Green",
+    value: "green",
+  },
+  {
+    label: "Hazel",
+    value: "hazel",
+  },
+  {
+    label: "Grey",
+    value: "grey",
+  },
+  {
+    label: "Other",
+    value: "other",
+  },
+];
+
+const gender = [
+  {
+    label: "Male",
+    value: "male",
+  },
+  {
+    label: "Female",
+    value: "female",
+  },
+  {
+    label: "Non-binary",
+    value: "non_binary",
+  },
+  {
+    label: "Other",
+    value: "other",
+  },
+];
+
+const unionStatus = [
+  {
+    label: "Union",
+    value: "union",
+  },
+  {
+    label: "Non-union",
+    value: "non_union",
+  },
+];
+
+const license = [
+  {
+    label: "Yes",
+    value: "yes",
+  },
+  {
+    label: "No",
+    value: "no",
+  },
+];
+
+
+export {
+  license,
+  unionStatus,
+  ageRange,
+  ethnicity,
+  gender,
+  eyeColor,
+  hairColor,
+  jobTypes,
+  skillTypes,
+};
