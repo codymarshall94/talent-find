69d4a26f22539df8999f11d70757b40181d93736
Cody Marshall <codymstuntsx@gmail.com>
2023-09-13 12:32:15 -0700

    ADDED: FILTER temp

---
+++ scenes/home/Home.tsx
@@ -0,0 +1,11 @@
+import Filter from "@/components/filter/Filter";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Filter />
+    </div>
+  );
+}
