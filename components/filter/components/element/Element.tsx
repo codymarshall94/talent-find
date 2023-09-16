d88e157d7685eb206ee0dd3a6d811e3fe261b604
Cody Marshall <codymstuntsx@gmail.com>
2023-09-15 17:04:17 -0700

    ADDED: GRID SPAN

--- components/filter/components/element/Element.tsx
+++ components/filter/components/element/Element.tsx
@@ -3,13 +3,17 @@ import React from "react";
 export default function Element({
   title,
   children,
+  gridSpan,
 }: {
   title: string;
   children: React.ReactNode;
+  gridSpan?: number;
 }) {
   return (
-    <div className="bg-gray-200 p-4 rounded-md">
-      <h3 className="font-semibold">{title}</h3>
+    <div
+      className={`bg-gray-200 p-2 rounded-md w-full col-span-1 md:col-span-${gridSpan}`}
+    >
+      <h3 className="font-semibold text-sm">{title}:</h3>
       {children}
     </div>
   );
