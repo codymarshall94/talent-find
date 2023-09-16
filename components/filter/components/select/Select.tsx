92f70a1a7e53d0ba2ec2347429d0a9942cc26c55
Cody Marshall <codymstuntsx@gmail.com>
2023-09-15 17:34:41 -0700

    UPDATED: gave styles and passed the arrays

--- components/filter/components/select/Select.tsx
+++ components/filter/components/select/Select.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 
-export default function Select({ array }: { array: any[] }) {
+export default function Select({
+  array,
+  handleChange,
+  name,
+  value = "",
+}: {
+  array: any[];
+  handleChange: (e: any) => void;
+  name: string;
+  value?: string;
+}) {
   return (
-    <select className="w-full mt-2">
+    <select
+      className="w-full mt-2 p-2 rounded-md bg-gray-100"
+      onChange={handleChange}
+      name={name}
+      id={name}
+      value={value}
+    >
+      <option value="" disabled>
+        Select {name}
+      </option>
       {array.map((item) => (
-        <option key={item.label} value={item.value}>
+        <option
+          key={item.label}
+          value={item.value}
+        >
           {item.label}
         </option>
       ))}
