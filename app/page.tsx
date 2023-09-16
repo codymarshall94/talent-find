7be6583c266022e769496cbe0c3388972f72185a
Cody Marshall <codymstuntsx@gmail.com>
2023-09-15 17:02:05 -0700

    REMOVED: unused flex classes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/scenes/home/Home";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen">
       <Home />
     </main>
   );
