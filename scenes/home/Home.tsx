04a433bc842925c325de0870a0e76ebb2f8e5439
Cody Marshall <codymstuntsx@gmail.com>
2023-09-15 17:34:14 -0700

    UPDATED: basic hero for home page

--- scenes/home/Home.tsx
+++ scenes/home/Home.tsx
@@ -1,11 +1,37 @@
-import Filter from "@/components/filter/Filter";
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Filter />
+    <div className="flex flex-col items-center p-4 mx-auto w-full">
+      <div className="bg-blue-600 sm:h-96 w-full md:w-3/4 rounded-md p-8 md:p-14 flex flex-col justify-between">
+        <div className="flex flex-col justify-between items-center">
+          <h1 className="text-xl md:text-4xl text-white font-semibold text-center">
+            Join Stuntlink today, and find your next stunt job!
+          </h1>
+          <p className="text-white text-center text-sm md:text-xl pt-4 md:w-3/4 mx-auto">
+            Stuntlink is the #1 place for stunt professionals to find work. We
+            have the largest database of stunt professionals in the world, and
+            we're growing every day.
+          </p>
+        </div>
+        <div className="flex flex-col justify-between items-center">
+          <button className="bg-black text-white rounded-md px-4 py-2 mx-auto block mt-4">
+            Link Up!
+          </button>
+          <div className="flex flex-row justify-center items-center mt-4">
+            <div className="flex flex-col items-center">
+              <h3 className="text-white font-bold md:text-xl">Recruiting</h3>
+              <p className="text-white text-sm pl-2">
+                Looking for a specific look?
+              </p>
+            </div>
+            <Link href="find" className="text-black font-bold border border-black rounded-md px-4 py-2 mx-auto block ml-2">
+              Find your Look
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
